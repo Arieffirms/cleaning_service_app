4bf59cda1771cfe7fa883f1d7d81fa937404284d
Arieffirms <arieffirmansyah0731@gmail.com>
2024-02-05 23:32:13 +0700

    menambahkan ui halaman

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cleaning_service_app/color_schema.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -6,7 +7,47 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple,
+      backgroundColor: deepPurple400,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 60,
+          ),
+          Text(
+            'Cleaning Service\nOnline',
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Rumah Sehat\nuntuk\nKehidupan Yang Sehat",
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 350,
+            width: MediaQuery.of(context).size.width,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/image/splash.png"),
+                  fit: BoxFit.cover),
+            ),
+          )
+        ],
+      ),
       // appBar: AppBar(),
     );
   }
